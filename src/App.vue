374e74752b9e8852906b30531402dbac53cfca8f
Daniel Morgan <me@danielmorgan.co.uk>
2017-06-07 22:55:31 +0100

    Set up mapbox

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
   <div id="app" class="container">
     <div class="row">
-      <div class="col s12 m6 offset-m3">
-        <div class="card blue-grey darken-3">
-          <div class="card-content white-text text-lighten-4">
+      <div class="col s12 l3">
+
+        <v-card class="blue-grey darken-1">
+          <div class="card-content white-text">
             <span class="card-title">
               <i class="material-icons">announcement</i>
               Hello World!
             </span>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt libero pariatur sit ut. Asperiores commodi consectetur.</p>
+            <p>I am a very simple v-card. I am good at containing small bits of information.</p>
           </div>
           <div class="card-action">
-            <a href="#">This is a link</a>
-            <a href="#">This is a link</a>
+            <a href="#">Action</a>
           </div>
-        </div>
+        </v-card>
+
+      </div>
+
+      <div class="col s12 l9">
+
+        <v-card>
+          <mapbox :accessToken="env.mapbox.accessToken"></mapbox>
+        </v-card>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import env from 'env';
+  import Mapbox from './components/Mapbox.vue';
+
   export default {
     name: 'app',
-    components: {}
+
+    components: { Mapbox },
+
+    data () {
+      return {
+        env: env
+      };
+    }
   };
 </script>
 
---
+++ src/components/Mapbox.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+    import mapboxgl from 'mapbox-gl';
+
+    export default {
+      props: [
+        'accessToken'
+      ],
+
+      mounted () {
+        mapboxgl.accessToken = this.accessToken;
+
+        this.map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v9'
+        });
+      }
+    };
+</script>
+
+<style lang="scss">
+    #map {
+        width: 100%;
+        height: 100%;
+        min-height: 500px;
+    }
+</style>
