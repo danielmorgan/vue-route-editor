7d0c71862c0781563ac7efe89715730bd2f52feb
Daniel Morgan <me@danielmorgan.co.uk>
2017-07-02 21:20:14 +0100

    Load editing track into local cache

--- src/components/FakeTrackLoader.vue
+++ src/components/FakeTrackLoader.vue
@@ -9,7 +9,8 @@
                 <div class="divider"></div>
             </li>
 
-            <li v-for="(track, index) in tracks">
+            <li v-for="(track, index) in tracks"
+                v-bind:class="{ active: isActive(index) }">
                 <a href="#" v-on:click.prevent="selectTrack(index)">
                     <i class="material-icons">timeline</i>
                     Track {{ index }}
@@ -20,16 +21,27 @@
 </template>
 
 <script>
+    import localforage from 'localforage';
+
     export default {
       computed: {
         tracks () {
           return this.$store.state.tracks;
+        },
+
+        selectedTrack () {
+          return this.$store.state.selectedTrack;
         }
       },
 
       methods: {
         selectTrack (index) {
           this.$store.commit('selectTrack', index);
+          localforage.setItem('editing', this.$store.state.selectedTrack);
+        },
+
+        isActive (index) {
+          return this.tracks[index] === this.selectedTrack;
         }
       }
     };
